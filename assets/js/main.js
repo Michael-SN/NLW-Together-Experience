a7d67a632331cd9ae2d6a669e5e480ddbbdb8f71
Michael Nascimento <michael0387nascimento@gmail.com>
2021-06-26 20:10:14 -0300

    Aula 5 - layout mobile finalizados, partindo para os breakpoints e Desktop

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,11 +18,10 @@ for (let link of links) {
 }
 
 // ALterando o HEADER no scroll
+function changeHeaderWhenScroll() {
+  const $header = document.querySelector('#header')
+  const navHeight = $header.offsetHeight
 
-const $header = document.querySelector('#header')
-const navHeight = $header.offsetHeight
-
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     // maior que a alura do header
     $header.classList.add('scroll')
@@ -30,7 +29,7 @@ window.addEventListener('scroll', function () {
     // menor que a alura do header
     $header.classList.remove('scroll')
   }
-})
+}
 
 // SWIPER CARROUSEL - TESTIMONTIALS
 // SWIPER FUNCTION
@@ -56,7 +55,26 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .logo, footer .brand, footer .social
 `,
   { interval: 100 }
 )
+
+// FUNCTION BACK TO TOP
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+// WHEN THIS PAGE HAS SCROLL EVENT
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
